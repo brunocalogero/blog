97fd2e4448d55a810dc6df99cb8d001b50f52349
brunocalogero <brunocalogero@hotmail.com>
2020-12-26 22:49:05 +0000

    change icon and website title for tab info

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'mystaticapp',
+    title: "Bruno's Website",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
