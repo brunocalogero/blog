d6f69d48ff30cba9e3a49b5dbbdf7cb1862bc1e5
brunocalogero <brunocalogero@hotmail.com>
2020-12-29 23:22:01 +0000

    index card changes flex, text centering , TODO: play more with width or switch to grid layout

--- components/IndexCard.vue
+++ components/IndexCard.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="border-2 hover:border-black rounded-xl px-16 py-12 
-    hover:shadow-lg transition duration-300 ease-out"> 
-    {{ title }}
+    hover:shadow-lg transition duration-300 ease-out
+    m-2
+    cursor-pointer
+    flex justify-center">
+    <div>{{ title }}</div>
   </div>
 </template>
 
@@ -15,7 +18,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,24 @@
 <template>
   <div class="container">
-    <div class="md:flex">
-      <IndexCard v-for="item in menuItems" :key="item.name" :title="item.name" class="m-2"/>
+    <div class="md:flex items-center">
+      <IndexCard v-for="item in menuItems" :key="item.name" :title="item.name"/>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
   data: () => ({
     menuItems: [
       {name: 'About'}, 
-      // {name: 'Stuff'},
-      {name: 'Blog (Coming Soon)'}, 
+      {name: 'Blog'}, 
     ]
-
   })
 }
 </script>
 
-
-
 <style lang="postcss">
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 </style>
