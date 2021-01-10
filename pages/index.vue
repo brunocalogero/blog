6c596e1e612693498314993765e5865b282b5ed8
brunocalogero <brunocalogero@hotmail.com>
2021-01-10 12:47:27 +0000

    pp quickie one

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <img class="avatar" src="../static/img/pp.jpeg">
     <div class="md:flex md:flex-wrap place-content-center md:w-8/12">
       <IndexCard v-for="item in menuItems" :key="item.name" :title="item.name"/>
     </div>
@@ -19,6 +20,10 @@ export default {
 
 <style lang="postcss">
 .container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+  @apply min-h-screen flex flex-col justify-center items-center mx-auto;
+}
+
+.avatar {
+  @apply object-cover rounded-full h-48 w-48 mb-40 shadow-lg; 
 }
 </style>
