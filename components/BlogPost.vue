0ea6d0249e0c4b580b137d98cde9ea9410502fb7
Bruno Calogero <bruno@riskledger.com>
2021-11-07 23:37:09 +0000

    blog post

---
+++ components/BlogPost.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLink 
+    :to="title" 
+    class="border-2 hover:border-black rounded-xl px-16 py-12 
+    hover:shadow-lg transition duration-300 ease-out
+    m-2
+    cursor-pointer
+    flex flex-col justify-center"
+  >
+    <div>{{ title }}</div>
+    <div>{{ createdAt | formatDate }}</div>
+  </NuxtLink>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: String,
+      required: true
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ pages/blog.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="container">
+      <div 
+        class="grid justify-items-center md:grid-cols-3 gap-4 place-items-center grid-cols-1"
+        v-for="page in pages" :key="page.title"  
+      >
+        <BlogPost
+          :title="page.title"
+          :createdAt="page.created_at"
+          :createdBy="page.created_by"
+          :tags="page.tags"
+        />
+        <BlogPost
+          :title="page.title"
+          :createdAt="page.created_at"
+          :createdBy="page.created_by"
+          :tags="page.tags"
+        />
+        <BlogPost
+          :title="page.title"
+          :createdAt="page.created_at"
+          :createdBy="page.created_by"
+          :tags="page.tags"
+        />
+        <BlogPost
+          :title="page.title"
+          :createdAt="page.created_at"
+          :createdBy="page.created_by"
+          :tags="page.tags"
+        />
+      </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  transition: 'about',
+  data() {
+    return {
+    };
+  },
+
+  async asyncData({ $content }) {
+    const pages = await $content("blog").sortBy('created_at', 'desc').fetch();
+    return {
+      pages
+    };
+  },
+  watch: {
+    pages(val) {
+      console.log(val);
+    },
+  },
+};
+</script>
+
+
+<style lang="postcss">
+.container {
+  @apply min-h-screen flex flex-col justify-center items-center mx-auto;
+}
+
+</style>
