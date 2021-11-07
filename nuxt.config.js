48d2b826a435cfebeb79113bab8247cda006b3d1
Bruno Calogero <bruno@riskledger.com>
2021-11-07 18:11:45 +0000

    highlight js setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import highlightjs from 'highlight.js'
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -11,7 +13,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Bruno Calogero' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -51,7 +53,20 @@ export default {
 
   // Content Configuration (https://content.nuxtjs.org/configuration)
   content: {
-    // markdown: {
-    // }
+    markdown: {
+      // Adds highlightJS code highlighting.
+      // A manual import of a specific style is needed in wanted vue file &
+      // overwrite of tailwindCSS classes might be required in tailwind.config.js
+      // e.g: import 'highlight.js/styles/atom-one-light.css'
+      //
+      highlighter(rawCode, lang) {
+        const highlightedCode = highlightjs.highlight(lang, rawCode).value
+
+        // We need to create a wrapper, because
+        // the returned code from highlight.js
+        // is only the highlighted code.
+        return `<pre><code class="hljs ${lang}">${highlightedCode}</code></pre>`
+      },
+    },
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,31 @@ module.exports = {
     // NOTE: could customize order using font-body class defined below but using mono default for the moment
     //   body: [],
     // },
+    typography: {
+      default: {
+        css: {
+          // HighlightJS requires us to override TailwindCSS classes
+          //
+          // Uses Notion markdown css for inline code
+          ':is(li, p) code': {
+            backgroundColor: 'rgba(135,131,120,0.15)',
+            borderRadius: '3px',
+            padding: '0.2em 0.4em',
+            color: '#EB5757',
+          },
+          // Removes back-ticks for inline code
+          'code::before': false,
+          'code::after': false,
+          // Removes TailwindCSS styling for code block & customizes it
+          pre: false,
+          'pre code': false,
+          code: {
+            color: false,
+            borderRadius: '5px',
+          },
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
