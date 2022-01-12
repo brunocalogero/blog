ded6254fde247bc026be0c1733cd3bdf7c0a66dd
Bruno Calogero <bruno@riskledger.com>
2022-01-12 18:13:56 +0000

    stylize header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="flex flex-col p-10 items-center">
-    <NuxtLink class="m-2 flex items-center" to="/">
+    <!-- TODO: try to animate arrow -->
+    <NuxtLink class="border-2 hover:border-black rounded-xl px-3 py-3 
+      hover:shadow-lg transition duration-300 ease-out
+      m-2 flex items-center
+      cursor-pointer" to="/">
       <ArrowLeftIcon class="back"/>
       <div class="ml-2">Home</div>
     </NuxtLink>
