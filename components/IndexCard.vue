6fa31b067e02c924bd1b195168b03463478922c4
Bruno Calogero <bruno@riskledger.com>
2022-01-12 18:14:12 +0000

    lint

--- components/IndexCard.vue
+++ components/IndexCard.vue
@@ -2,10 +2,10 @@
   <NuxtLink 
     :to="title" 
     class="border-2 hover:border-black rounded-xl px-16 py-12 
-    hover:shadow-lg transition duration-300 ease-out
-    m-2
-    cursor-pointer
-    flex w-1/4 justify-center">
+      hover:shadow-lg transition duration-300 ease-out
+      m-2
+      cursor-pointer
+      flex w-1/4 justify-center">
     <div>{{ title | capitalize }}</div>
   </NuxtLink>
 </template>
