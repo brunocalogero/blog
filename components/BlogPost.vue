e99a34143961185c52b080cb1fa49a0ac685b911
Bruno Calogero <bruno@riskledger.com>
2022-05-22 16:51:16 +0200

    remove old slug component and use new underscored slug

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink 
-    :to="title" 
+    :to="{ name: 'blog-slug', params: { slug: post.slug } }" 
     class="border-2 hover:border-black rounded-xl px-16 py-12 
     hover:shadow-lg transition duration-300 ease-out
     m-2
@@ -9,29 +9,17 @@
     text-center
     "
   >
-    <div>{{ title }}</div>
-    <div class="mt-2">{{ createdAt | formatDate }}</div>
+    <div>{{ post.title }}</div>
+    <div class="mt-2">{{ post.created_at | formatDate }}</div>
   </NuxtLink>
 </template>
 <script>
 export default {
   props: {
-    title: {
-      type: String,
+    post: {
+      type: Object,
       required: true
-    },
-    createdAt: {
-      type: String,
-      required: true
-    },
-    createdBy: {
-      type: String,
-      required: true,
-    },
-    tags: {
-      type: Array,
-      required: true,
-    },
+    }
   },
 }
 </script>
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto px-10 py-32">
+    <article class="mx-auto prose prose-sm sm:prose lg:prose-lg box-content md:px-8">
+      <nuxt-content :document="post" />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+
+      const post = await $content('blog', params.slug).fetch()
+
+    return { post }
+  }
+}
+</script>
